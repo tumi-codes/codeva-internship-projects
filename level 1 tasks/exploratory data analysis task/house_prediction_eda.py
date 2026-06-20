e0c33b4f2f15c36eecd7572da575519ca7672b20
tumidev38 <tumidev38@gmail.com>
2026-06-20 00:46:27 -0700

    updated the file structure for easier navigation

--- level 1 tasks/exploratory data analysis task/house_prediction_eda.py
+++ level 1 tasks/exploratory data analysis task/house_prediction_eda.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 pd_sentiment = pd.read_csv("Data Set For Task/4) house Prediction Data Set.csv", sep=r"\s+",header=None)
 pd_sentiment.columns = ['CRIM','ZN','INDUS','CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
-print(pd_sentiment.head())
 
 # cleaning the dataset
 pd_sentiment.dropna(inplace=True)  
